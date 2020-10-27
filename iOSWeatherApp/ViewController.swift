7a37f4b5bd42ddf2ffcc45e0842a5f2e8a56b15e
Jimin Huh <jiminhuh95@gmail.com>
2020-10-26 21:39:51 -0500

    Initial Commit

---
+++ iOSWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSWeatherApp
+//
+//  Created by Jimin Huh on 10/26/20.
+//  Copyright © 2020 Jimin Huh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
